8fa7bcb31bf7429f89987f5ec98070a1834cc0c3
cv <cv-us@pm.me>
2025-04-29 13:13:17 -0700

    header text

--- src/components/SimpleLayout.jsx
+++ src/components/SimpleLayout.jsx
@@ -4,10 +4,10 @@ export function SimpleLayout({ title, intro, children }) {
   return (
     <Container className="mt-16 sm:mt-32">
       <header className="max-w-full">
-        <h1 className="text-4xl font-bold tracking-tight text-yellow-400 dark:text-brown-100 sm:text-5xl">
+        <h1 className="text-4xl font-bold tracking-tight text-lime-400 dark:text-lime-100 sm:text-5xl">
           {title}
         </h1>
-        <p className="mt-6 text-base text-grey-600 dark:text-brown-100">
+        <p className="mt-6 text-base text-brown-600 dark:text-brown-100">
           {intro}
         </p>
       </header>
