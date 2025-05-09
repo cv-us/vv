fce10f20895ef913bba5f397e803c4766b1a72a7
cv <cv-us@pm.me>
2025-05-08 17:46:58 -0700

    footer padding top

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import { Header } from '@/components/Header'
 export function Layout({ children }) {
   return (
     <>
-      <div className="relative flex w-full flex-col">
+      <div className="relative flex w-full flex-col align-middle">
         <Header />
         <main className="flex-auto">{children}</main>
         <Footer />
